beeef06acc85f97f9f8c854a155435aa066d9804
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-10-12 13:26:56 -0500

    chore: Initial commit

---
+++ src/main/java/pe/upc/learningcenterplatform/LearningCenterPlatformApplication.java
@@ -0,0 +1,13 @@
+package pe.upc.learningcenterplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningCenterPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningCenterPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/upc/learningcenterplatform/LearningCenterPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upc.learningcenterplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningCenterPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
