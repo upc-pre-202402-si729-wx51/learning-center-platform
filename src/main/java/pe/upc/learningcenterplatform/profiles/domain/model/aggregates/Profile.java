1ae4c2c8b7f285f5be0ae8ba73f9cb01ecaf0073
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-10-18 19:45:00 -0500

    feat(profiles): added profile aggregate

--- src/main/java/pe/upc/learningcenterplatform/profiles/domain/model/aggregates/Profile.java
+++ src/main/java/pe/upc/learningcenterplatform/profiles/domain/model/aggregates/Profile.java
@@ -1,12 +1,8 @@
 package pe.upc.learningcenterplatform.profiles.domain.model.aggregates;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.domain.AbstractAggregateRoot;
 import pe.upc.learningcenterplatform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 
 @Entity
